e78e1824e2ff635df2db0563c7c9ddad2eacbaf5
Amwighio <159983597+manan7866@users.noreply.github.com>
2024-06-11 06:01:34 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//
+const randomNumber = Math.floor(Math.random() * 6);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number Between 0 to 6 : ",
+    }
+]);
+console.log(answers);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congratulations you guessed right number.");
+}
+else {
+    console.log("sorry you guessed wrong number");
+}
